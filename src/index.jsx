73cc48f188e01e09cbd9cab8c08ec41b45d50cbf
Mireia <66952824+miiree12lb@users.noreply.github.com>
2024-12-13 14:34:56 +0100

    video promo

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 // @ts-ignore
 import './css/root.css';
 import Footer from "./Footer.jsx";
 
 function Root() {
+    const isDesktop = useMediaQuery({ query: "(min-width: 801px)" });
+    const width = isDesktop ? "50" : "80";
+
     return (
         <BrowserRouter>
             <div id="view">
                 <h1>MenuMentor</h1>
+                <div 
+                    id="video-holder"
+                    style={{
+                        width: `${width}vw`,
+                        height: `${(parseFloat(width) * 9) / 16}vw`
+                    }}
+                > 
+                    <iframe 
+                        src="https://drive.google.com/file/d/1t5o0a813PEGNkQIZODBMwaUzxciKhZDD/preview"
+                        allow="autoplay; encrypted-media;"
+                        allowFullScreen
+                        width="100%"
+                        height="100%"
+                    >
+                    </iframe>    
+                </div>
+
                 <h2>Introduction</h2>
                 <p>
                     This is a cross-platform desktop application created using Java. I developed this project predominantly during my 11th and 12th grades in high school. 
@@ -33,16 +54,16 @@ function Root() {
                 <h2>Installation Requirements</h2>
                 <p>This application requires Java 17 and JVM 17 for proper functionality. To install Java 17 and JVM 17:</p>
                 <ol>
-                    <li>Visit the official Java website: <Link to="https://www.oracle.com/java/technologies/downloads/#java17?er=221886">Java Downloads</Link></li>
+                    <li>Visit the official Java website: <a href="https://www.oracle.com/java/technologies/downloads/#java17?er=221886" target="_blank" rel="noopener noreferrer">Java Downloads</a></li>
                     <li>Download and install Java 17 for your operating system.</li>
                     <li>Ensure that the installed Java version matches the application's requirements.</li>
                 </ol>
 
                 <h2>How to install</h2>
                 <ol>
-                    <li><Link to="https://github.com/miiree12lb/MenuMentor/archive/main.zip">Download Zip</Link></li>
+                    <li><a href="https://github.com/miiree12lb/MenuMentor/archive/main.zip" target="_blank" rel="noopener noreferrer">Download Zip</a></li>
                     <li>Unzip the folder in the desired folder on your computer.</li>
-                    <li>Open the config.txt file and write the location in the computer where you want to save the SQLite database (Here's an example on windows: C:\Users\mirei\Desktop\MenuMentor\InternalDB.db).</li>
+                    <li>Open the config.txt file and write the location on the computer where you want to save the SQLite database (Example on Windows: C:\Users\mirei\Desktop\MenuMentor\InternalDB.db).</li>
                     <li>Compile and run the code in a terminal.</li>
                 </ol>
             </div>
@@ -58,3 +79,5 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
     createRoot(rootElement).render(<Root />);
 }
+
+export default Root;
